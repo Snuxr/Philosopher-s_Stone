6b9d1598369e085612d6806953f617cc16e5f03a
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-04-20 03:22:35 +0530

    feat: setup baseUrl for rtk-q Api

--- src/store/apis/productsApi.js
+++ src/store/apis/productsApi.js
@@ -3,7 +3,7 @@ import {createApi ,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 const productApi = createApi({
     reducerPath:'products',
     baseQuery: fetchBaseQuery({
-        baseUrl:''
+        baseUrl:'http://localhost:5000'
     }),
     endpoints: (builder) => {
         return {
