7f84625f5fddeb255d5c5a7ec9029b3a57985f69
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-04-19 13:05:00 +0530

    feat(ProductOverview comp): create ProductOverview comp and some styling changes in other comps

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import BannerSlider from "../Components/Banners/BannerSlider"
 import CategoryBox from "../Components/Categories/CategoryBox"
-import { faker } from "@faker-js/faker"
+import ProductOverview from "../Components/Products/ProductOverview"
+import { useFetchProductsQuery } from "../store/apis/productsApi"
 
 const Home = () => {
-    
+
     const leftBanner_Data = [
         {
             id: 1,
@@ -27,7 +28,7 @@ const Home = () => {
             route: "/products/samsung galaxy s24 ultra",
         },
     ]
-    
+
     const rightBanner_Data = [
         {
             id: 4,
@@ -48,101 +49,68 @@ const Home = () => {
             link: "https://www.apple.com/newsroom/images/2023/10/apple-unveils-new-macbook-pro-featuring-m3-chips/article/Apple-MacBook-Pro-2up-231030_Full-Bleed-Image.jpg.large.jpg",
             name: "MacBook Pro M3",
             title: "Apple the new MacBook Pro featuring the M3 family of chips, making the world's best pro laptop even better",
-            route: "/products/macbook pro M3",            
+            route: "/products/macbook pro M3",
         },
     ]
 
     const Categories = [
         {
-            id: faker.string.uuid(),
-            input: 'Dope Gadgets',
-            options:[
-                'TP-7',
-                'PO-33 KO',
-                'Apple Vision Pro',
-                'R1',
-                'Meta Quest 3',
-            ]
+            id: 1,
+            title: 'Clothes',
         },
         {
-            id: faker.string.uuid(),
-            input: 'Phones',
-            options:[
-                'Samsung Galaxy S24 Ultra',
-                'Iphone 15 Pro',
-                'Noting Phone (1)',
-                'Xiaomi 14 Ultra',
-                'Oneplus 12R',
-            ]
-        },
-        {
-            id: faker.string.uuid(),
-            input: 'Earphones',
-            options:[
-                'Apple AirPods Pro 2',
-                'Samsung Galaxy Buds 2 Pro',
-                'CMF by Nothing Buds',
-                'Bose QuietComfort Earbuds 2',
-            ]
-        },
-        {
-            id: faker.string.uuid(),
-            input: 'Laptops',
-            options:[
-                'MacBook Pro M3',
-                'Asus Zenbook 13 OLED',
-                'Macbook Air M3',
-                'Dell XPS 15 OLED',
-                'Lenovo Chromebook Duet 3',
-                'Asus ROG Zephyrus G14',
-                'Acer Swift 5',
-            ]
-        },
-        {
-            id: faker.string.uuid(),
-            input: 'Watches',
-            options:[
-                'Apple Watch SE',
-                'Garmin Venu 3S',
-                'Amazfit Balance',
-                'Withings ScanWatch 2',
-            ]
+            id: 2,
+            title: "Electronics",
         },
         {
             id: 3,
-            input: 'Accessories',
-            options:[
-                'Chargers',
-                'Layers',
-                'Phone Covers',
-                'Desktop Accessories',
-            ]
+            title: "Furniture",
         },
         {
-            id: faker.string.uuid(),
-            input: 'Services',
-            options:[
-                'Customer Care',
-                'Gadgets Insurance',
-                'EMI',
-            ]
+            id: 4,
+            title: "Shoes",
         },
+        {
+            id: 5,
+            title: "Miscellaneous",
+        }
     ]
 
-    const content = Categories.map((category) => {
+    const { data, isError, isSuccess } = useFetchProductsQuery()
+
+    const CategoryBoxContent = Categories.map((category) => {
         return (
-            <CategoryBox key={category.id} options={category.options} >{category.input}</CategoryBox>
+            <CategoryBox key={category.id} >{category.title}</CategoryBox>
         )
     })
 
+    let ProductOverviewContent;
+
+    if (isError) {
+        ProductOverviewContent = <img className="w-96 m-10" src='https://httpstatusdogs.com/img/402.jpg' alt="402" />
+    }
+
+
+    if (!isError && isSuccess) {
+        ProductOverviewContent = data.map((product) => {
+            if (product.images.length > 2) {
+                let filteredTitle =product.title.replace('Sleek', '')
+                return (
+                    <ProductOverview key={product.id} product={product} >{filteredTitle}</ProductOverview>
+                )
+            }
+        })
+    }
+
     return (
-        <div className="mt-8 mb-8 ">
+        <div className="mt-8 mb-8">
             <div className="grid grid-cols-2 gap-2">
                 <BannerSlider data={leftBanner_Data} />
                 <BannerSlider data={rightBanner_Data} />
             </div>
-            <div className="flex flex-col m-6">
-               <div className="flex flex-row">{content}</div>
+            <div className="flex flex-col m-6 gap-8">
+                <div className="flex flex-row">{CategoryBoxContent}</div>
+                <div className="flex flex-row flex-wrap justify-start gap-8">{ProductOverviewContent}</div>
             </div>
         </div>
     )
