f353c51f6119b3ad795c1af631d35147253ec113
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-05-03 07:38:37 +0530

    feat: setup cartApi and cartSlice with store inside index.js and export all the actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { productApi } from "./apis/productsApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { categoriesApi } from "./apis/categoriesApi";
+import { productApi } from "./apis/productsApi";
+import { cartApi } from "./apis/cartApi";
 import { prodCateReducer } from "./slices/prodCateSlice";
+import { cartReducer } from "./slices/cartSlice";
 
 export const store = configureStore({
     reducer: {
         prodCateSlice: prodCateReducer,
+        cartSlice: cartReducer,
         [productApi.reducerPath]: productApi.reducer,
-        [categoriesApi.reducerPath]: categoriesApi.reducer,
+        [cartApi.reducerPath]: cartApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
                 .concat(productApi.middleware)
-                .concat(categoriesApi.middleware)
+                .concat(cartApi.middleware)
     }
 })
 
 setupListeners(store.dispatch)
 
 export { useFetchProductsQuery } from "./apis/productsApi"
-export { useFetchCategoriesQuery } from "./apis/categoriesApi"
+export { useFetchCartQuery, useAddToCartMutation  } from "./apis/cartApi"
 export { AddManyProducts } from "./slices/prodCateSlice"
+export { AddToCart, AddToCartAll } from "./slices/cartSlice"
