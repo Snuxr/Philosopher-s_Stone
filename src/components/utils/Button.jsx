3c427b7663ac63e3a91b2574931befd7c46ddef3
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-05-03 07:47:34 +0530

    feat: create a Button comp

--- src/components/utils/Button.jsx
+++ src/components/utils/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({children}) => {
+const Button = ({children, ...rest}) => {
   return (
-    <div>
-      <button className="p-2 rounded-lg text-sm font-normal text-gray-800 bg-gray-100 bg-opacity-95 hover:bg-indigo-500 hover:bg-opacity-30 hover:text-gray-100 shadow-full shadow-indigo-500/50">{children}</button>
+    <div {...rest}>
+      <button className="p-2 rounded-lg text-sm font-normal text-gray-800 bg-gray-100 bg-opacity-95 hover:bg-gray-200 hover:bg-opacity-20  hover:text-gray-100 shadow-full shadow-indigo-500/50">{children}</button>
     </div>
   )
 }
