60a105e47cc42979e8049723201e82b363ff01df
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-05-03 12:32:18 +0530

    feat: Add Cart Page and fetch data from cartSlice

---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from "react-redux"
+import ExpandedProd from "../Components/ExpandedProducts/ExpandedProd"
+import CategoryList from "../Components/Categories/CategoryList"
+import Button from "../Components/Utils/Button"
+
+const Cart = () => {
+  const {cart} = useSelector((state) => {
+    return state.cartSlice
+  })
+
+  const cartContent = cart.map((cartProd) => {
+    return <ExpandedProd key={cartProd.id} product={cartProd.product} />
+})
+  return (
+    <div className="mt-8 mb-8 mr-6 ml-6 flex flex-col items-center gap-16">
+      <CategoryList/>
+      <div className="flex flex-col items-center gap-4">
+        {cartContent}
+      </div>
+      <div className="w-full flex flex-row justify-end">
+        <Button className="m-2 pl-10 pr-10 text-base font-extralight">Proceed to Buy  ( {cartContent.length} item )</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Cart
--- src/components/utils/Button.jsx
+++ src/components/utils/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({children, ...rest}) => {
   const classes = classnames(rest.className);
 
   return (
-      <button {...rest} className={twMerge("p-2 rounded-lg text-sm font-normal text-slate-300 bg-white bg-opacity-10 hover:bg-opacity-90 hover:text-gray-950 hover:shadow-full hover:shadow-indigo-500/50", classes)}>{children}</button>
+      <button {...rest} className={twMerge("p-2 rounded-lg text-sm font-normal text-slate-300 bg-white bg-opacity-10 hover:bg-opacity-90 hover:text-gray-950", classes)}>{children}</button>
   )
 }
 
