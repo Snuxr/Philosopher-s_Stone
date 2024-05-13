32722c73439db987da422cf9778495fa7a963e9d
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-05-13 21:01:52 +0530

    feat: Add flex Property in Button comp

--- src/components/utils/Button.jsx
+++ src/components/utils/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({children, ...rest}) => {
   const classes = classnames(rest.className);
 
   return (
-      <button {...rest} className={twMerge("p-2 rounded-lg text-sm font-normal text-slate-300 bg-white bg-opacity-10 hover:bg-opacity-90 hover:text-gray-950", classes)}>{children}</button>
+      <button {...rest} className={twMerge("flex flex-row justify-center items-center gap-2 p-2 rounded-lg text-sm font-normal text-slate-300 bg-white bg-opacity-10 hover:bg-opacity-90 hover:text-gray-950", classes)}>{children}</button>
   )
 }
 
